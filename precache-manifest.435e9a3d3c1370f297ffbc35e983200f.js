48781f6d172ddcebc810c78141abcc08a2db0d65
IGOR <igornesan@gmail.com>
2020-07-04 19:17:18 +0300

    Updates

---
+++ precache-manifest.435e9a3d3c1370f297ffbc35e983200f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44535dc0b6f72cc38b5d2d833005fd00",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "507e85ad46b713d666d0",
+    "url": "/contactmanager/static/css/2.7fef7f77.chunk.css"
+  },
+  {
+    "revision": "507e85ad46b713d666d0",
+    "url": "/contactmanager/static/js/2.c3093e8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.c3093e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d938ad0272720f4ae9",
+    "url": "/contactmanager/static/js/main.40e13ca3.chunk.js"
+  },
+  {
+    "revision": "e155b645805d8202c894",
+    "url": "/contactmanager/static/js/runtime-main.8912a38d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/contactmanager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/contactmanager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/contactmanager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/contactmanager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/contactmanager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
